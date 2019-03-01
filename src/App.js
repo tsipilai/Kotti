3755cb2f2693e578c35f57e5e7d3c3143c11e407
tsipilai <tommi.sipilainen@aucor.fi>
2019-03-01 23:04:44 +0200

    Basic fetch for home and work lines

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
+const Timestamp = require('react-timestamp');
+
+const homeUrl = 'http://data.foli.fi/siri/sm/495/pretty';
+const workUrl = 'http://data.foli.fi/siri/sm/1031/pretty';
+let data = "";
+
 class App extends Component {
+
+  state = {
+    homeStopData: "",
+    workStopData: "",
+  };
+
+  async componentDidMount() {
+    data = fetch(homeUrl);
+    data.then(response => response.json()) 
+    .then(json => {   
+      console.log(Array.isArray(json.result))    
+      console.log(json.result.map((result) => result))            
+         this.setState({
+          homeStopData: json.result
+         }
+       );
+    })
+    data = fetch(workUrl);
+    data.then(response => response.json()) 
+    .then(json => {   
+      console.log(Array.isArray(json.result))    
+      console.log(json.result.map((result) => result))            
+         this.setState({
+          workStopData: json.result
+         }
+       );
+    })
+    .catch(error => {                  
+    });
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        Kotoa Töihi
+        {this.state.homeStopData && 
+          this.state.homeStopData.map((result, i) =>
+            <div key={i}>
+              <span>{result.lineref} </span> 
+              <Timestamp time={result.expectedarrivaltime} />
+              <span>Auto: {result.vehicleref}</span>
+            </div>
+          )} 
+         Saapuu töihi
+         {this.state.workStopData && 
+          this.state.workStopData.map((result, i) =>  
+            <div key={i}>
+              <span>{result.lineref} </span> 
+              <Timestamp time={result.expectedarrivaltime} />
+              <span>Auto: {result.vehicleref}</span>
+            </div>
+          )}  
         </header>
       </div>
     );
