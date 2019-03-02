f8e841b7a633104b6cc8f88ae2d9db47f43e991a
tsipilai <tommi.sipilainen@aucor.fi>
2019-03-02 20:26:37 +0200

    node sass, refresh, styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
 
 const Timestamp = require('react-timestamp');
 
 const homeUrl = 'http://data.foli.fi/siri/sm/495';
 const workUrl = 'http://data.foli.fi/siri/sm/1031';
+const toHomeUrl = 'http://data.foli.fi/siri/sm/475';
+const fromWorkUrl = 'http://data.foli.fi/siri/sm/1047';
 let data = "";
 
 class App extends Component {
 
   state = {
-    homeStopData: "",
-    workStopData: "",
+    fromHomeData: "",
+    toWorkData: "",
+    toHomeData: "",
+    fromWorkData: "",
   };
 
-  function getData() {
+   getData = () => {
+    console.log('moi')
     data = fetch(homeUrl);
     data.then(response => response.json()) 
     .then(json => {            
          this.setState({
-          homeStopData: json.result
+          fromHomeData: json.result
          }
        );
     })
@@ -27,7 +32,27 @@ class App extends Component {
     data.then(response => response.json()) 
     .then(json => {   
          this.setState({
-          workStopData: json.result
+          toWorkData: json.result
+         }
+       );
+    })
+    .catch(error => {                  
+    });
+    data = fetch(fromWorkUrl);
+    data.then(response => response.json()) 
+    .then(json => {   
+         this.setState({
+          fromWorkData: json.result
+         }
+       );
+    })
+    .catch(error => {                  
+    });
+    data = fetch(toHomeUrl);
+    data.then(response => response.json()) 
+    .then(json => {   
+         this.setState({
+          toHomeData: json.result
          }
        );
     })
@@ -35,8 +60,12 @@ class App extends Component {
     });
   }
 
+  componentWillMount() {
+    this.getData();
+    this.refresh = setInterval(() => this.getData(), 10000);
+  }
+
   async componentDidMount() {
-    getData();
   }
   render() {
 
@@ -45,16 +74,37 @@ class App extends Component {
         <header className="App-header">
 
         </header>
-        Kotoa Töihi
-        {this.state.homeStopData && 
-          this.state.homeStopData.filter((home) => home.lineref == 60 ).map((home) =>
-            <div key={home.datedvehiclejourneyref}>
+        <h1>Kotoa Töihi</h1>
+        {this.state.fromHomeData && 
+          this.state.fromHomeData.filter((home) => home.lineref == 60 ).map((home) =>
+            <div className="buswrap" key={home.datedvehiclejourneyref}>
+            <div className="busfrom">
               <span className="title">Lähtee: </span>
               <span>{home.lineref} </span> 
               <Timestamp time={home.expectedarrivaltime} />
-              {this.state.workStopData && 
-                this.state.workStopData.filter((work) => work.datedvehiclejourneyref === home.datedvehiclejourneyref).map((work) =>
-                <div key={work.datedvehiclejourneyref}>
+            </div>
+
+              {this.state.toWorkData && 
+                this.state.toWorkData.filter((work) => work.datedvehiclejourneyref === home.datedvehiclejourneyref).map((work) =>
+                <div className="busto" key={work.datedvehiclejourneyref}>
+                  <span>Perillä: <Timestamp time={work.destinationaimedarrivaltime} format="time"/></span>
+                </div> 
+                )}
+            </div>
+          )} 
+        <h1>Töist Kotti</h1>
+        {this.state.fromWorkData && 
+          this.state.fromWorkData.filter((home) => home.lineref == 60 ).map((home) =>
+            <div className="buswrap" key={home.datedvehiclejourneyref}>
+            <div className="busfrom">
+              <span className="title">Lähtee: </span>
+              <span>{home.lineref} </span> 
+              <Timestamp time={home.expectedarrivaltime} />
+            </div>
+
+              {this.state.toHomeData && 
+                this.state.toHomeData.filter((work) => work.datedvehiclejourneyref === home.datedvehiclejourneyref).map((work) =>
+                <div className="busto" key={work.datedvehiclejourneyref}>
                   <span>Perillä: <Timestamp time={work.destinationaimedarrivaltime} format="time"/></span>
                 </div> 
                 )}
