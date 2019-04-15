019d6bd57b4b0f6038a9160b74224a5d980f33c9
tsipilai <tommi.sipilainen@gmail.com>
2019-04-15 13:53:01 +0300

    Switch animations

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
   Switch,
   Link
 } from 'react-router-dom';
+import { AnimatedSwitch } from 'react-router-transition';
 
 import Schedule from './components/schedule/schedule';
 
@@ -30,6 +31,32 @@ const personB = {
   }, 
 };
 
+function mapStyles(styles) {
+  return {
+    opacity: styles.opacity,
+    transform: `scale(${styles.scale})`,
+  };
+}
+
+// child matches will...
+const switchTransition = {
+  // start in a transparent, upscaled state
+  atEnter: {
+    opacity: 0,
+    scale: 1.2,
+  },
+  // leave in a transparent, downscaled state
+  atLeave: {
+    opacity: 0,
+    scale: 1.2,
+  },
+  // and rest at an opaque, normally-scaled state
+  atActive: {
+    opacity: 1,
+    scale: 1,
+  },
+};
+
 
 class App extends Component {
 
@@ -39,20 +66,26 @@ class App extends Component {
       <Router>
         <div className="App">
           <header className="App-header">
-            <Link to="/">
+            <Link to="/" className="nav-item">
               Kotti
             </Link>
-            <Link to="/tommi">
+            <Link to="/tommi" className="nav-item">
               Tommi
             </Link>
-            <Link to="/leea">
+            <Link to="/leea" className="nav-item">
               Leea
             </Link>
           </header>
-          <Switch>
+          <AnimatedSwitch
+            atEnter={switchTransition.atEnter}
+            atLeave={switchTransition.atLeave}
+            atActive={switchTransition.atActive}
+            mapStyles={mapStyles}
+            className="switch-wrapper"
+          >
             <Route path="/tommi" render={(props) => <Schedule {...props} name={'tommi'} busstops={personA} />} />
             <Route path="/leea"  render={(props) => <Schedule {...props} name={'leea'} busstops={personB} />} />
-          </Switch>
+          </AnimatedSwitch>
         </div>
       </Router>
     );
